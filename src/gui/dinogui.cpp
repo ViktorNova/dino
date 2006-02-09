8e5c40e786e23e2ad9740dd9f208f4d55b00d9ab
Lars Luthman <lars.luthman@gmail.com>
2006-02-09 03:30:16 +0000

    Cleaned up the about dialog code

--- src/gui/dinogui.cpp
+++ src/gui/dinogui.cpp
@@ -44,12 +44,8 @@ DinoGUI::DinoGUI(int argc, char** argv, RefPtr<Xml> xml)
   
   m_window = w<Gtk::Window>("main_window");
   m_about_dialog = w<AboutDialog>("dlg_about");
-  string copyright = m_about_dialog->get_copyright();
-  size_t n = copyright.find("CR_YEAR");
-  if (n != string::npos) {
-    copyright.replace(n, strlen("CR_YEAR"), CR_YEAR);
-    m_about_dialog->set_copyright(copyright);
-  }
+  m_about_dialog->set_copyright("\u00A9 " CR_YEAR 
+				" Lars Luthman <larsl@users.sourceforge.net>");
   
   init_pattern_editor();
   init_sequence_editor();

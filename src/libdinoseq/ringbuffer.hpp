f161c5b1add0bdf321496640e5b453fc7a1bd302
Lars Luthman <lars.luthman@gmail.com>
2006-02-13 03:34:59 +0000

    Cleaned up the ringbuffer implementation

--- src/libdinoseq/ringbuffer.hpp
+++ src/libdinoseq/ringbuffer.hpp
@@ -9,7 +9,8 @@ namespace Dino {
 
   /** This template class is an implementation of a ringbuffer (or rather
       a circular queue). It can be used to send objects between threads
-      in a realtime safe way. */
+      in a realtime safe way, assuming that read and write operations on 
+      int variables are atomic. */
   template <class T> class Ringbuffer {
   public:
   
@@ -18,28 +19,28 @@ namespace Dino {
     ~Ringbuffer();
   
     /** This pushes another object onto the queue. */
-    bool push(const T& atom);
+    bool push(const T& thing);
     /** This removes the last object from the queue and copies it to 
-	@c atom. */
-    bool pop(T& atom);
+	@c thing. */
+    bool pop(T& thing);
   
   protected:
   
     volatile int m_read_pos;
     volatile int m_write_pos;
-    int m_size;
+    const int m_size;
   
-    volatile T* m_data;
+    T* volatile m_data;
 
   };
 
 
-  template <class T> Ringbuffer<T>::Ringbuffer(int size) {
+  template <class T> Ringbuffer<T>::Ringbuffer(int size) 
+    : m_read_pos(0),
+      m_write_pos(0),
+      m_size(size),
+      m_data(new T[m_size]) {
     dbg1<<"Creating ringbuffer for "<<demangle(typeid(T).name())<<endl;
-    m_data = new T[size];
-    m_read_pos = 0;
-    m_write_pos = 0;
-    m_size = size;
   }
 
 
@@ -49,19 +50,19 @@ namespace Dino {
   }
 
 
-  template <class T> bool Ringbuffer<T>::push(const T& atom) {
+  template <class T> bool Ringbuffer<T>::push(const T& thing) {
     if ((m_write_pos + 1) % m_size == m_read_pos)
       return false;
-    m_data[m_write_pos] = atom;
+    m_data[m_write_pos] = thing;
     m_write_pos = (m_write_pos + 1) % m_size;
     return true;
   }
 
 
-  template <class T> bool Ringbuffer<T>::pop(T& atom) {
+  template <class T> bool Ringbuffer<T>::pop(T& thing) {
     if (m_write_pos == m_read_pos)
       return false;
-    atom = m_data[m_read_pos];
+    thing = m_data[m_read_pos];
     m_read_pos = (m_read_pos + 1) % m_size;
     return true;
   }

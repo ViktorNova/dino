94f56768d8b661b14f642a4ea520d35565b19570
Lars Luthman <lars.luthman@gmail.com>
2006-04-24 23:31:02 +0000

    Some info editor updates, still not working correctly

--- src/gui/infoeditor.cpp
+++ src/gui/infoeditor.cpp
@@ -1,3 +1,5 @@
+#include <gtkmm/textbuffer.h>
+
 #include "infoeditor.hpp"
 #include "song.hpp"
 
@@ -22,14 +24,35 @@ InfoEditor::InfoEditor(BaseObjectType* cobject,
 
 void InfoEditor::set_song(Song* song) {
   m_song = song;
+  
+  /* ugly hack #1: TextBuffer::set_text is ambiguous so we need to disambiguate
+     it like this */
+  void (TextBuffer::*set_text)(const ustring&) = &TextBuffer::set_text;
+  
+  /* ugly hack #2: we need the actual TextBuffer pointer instead of the RefPtr,
+     so we have to use operator->() to get it */  
+  TextBuffer* buf = m_text_info->get_buffer().operator->();
+  
+  /* ugly hack #3: TextBuffer::get_text is ambiguous too */
+  //ustring (TextBuffer::*get_text)(bool) = &TextBuffer::get_text;
+  
   m_song->signal_title_changed.connect(mem_fun(m_ent_title, &Entry::set_text));
   m_song->signal_author_changed.connect(mem_fun(m_ent_author,&Entry::set_text));
+  m_song->signal_info_changed.connect(mem_fun(buf, set_text));
+  
   slot<void> set_title = compose(mem_fun(*m_song, &Song::set_title),
 				 mem_fun(m_ent_title, &Entry::get_text));
-  m_ent_title->signal_changed().connect(set_title);
   slot<void> set_author = compose(mem_fun(*m_song, &Song::set_author),
 				  mem_fun(m_ent_author, &Entry::get_text));
+  /*slot<void> set_info = compose(mem_fun(*m_song, &Song::set_info),
+    bind(mem_fun(buf, get_text), true));*/
+
+  m_ent_title->signal_changed().connect(set_title);
   m_ent_author->signal_changed().connect(set_author);
+  //m_text_info->get_buffer()->signal_changed().connect(set_info);
+  
+  // XXX only update the info when needed, and only update it once every second
+  // or so
 }
 
 

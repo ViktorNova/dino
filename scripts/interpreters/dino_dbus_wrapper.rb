514a2d983e8d7f8a2bfa0211681d695f44e9ac01
Lars Luthman <lars.luthman@gmail.com>
2007-07-20 18:22:31 +0000

    Added (unfinished) Ruby interpreter

---
+++ scripts/interpreters/dino_dbus_wrapper.rb
@@ -0,0 +1,8 @@
+require 'dbus';
+
+bus = DBus::SessionBus.instance;
+dino_service = bus.service("org.nongnu.dino");
+dino_object = dino_service.object("/");
+dino_object.introspect;
+song = dino_object["org.nongnu.dino.Song"];
+seq = dino_object["org.nongnu.dino.Sequencer"]; 0

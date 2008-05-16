50a84f935565d07e81585698da70e95b120364e2
Lars Luthman <lars.luthman@gmail.com>
2008-05-16 19:23:09 +0200

    Added even more Doxygen comments for the D-Bus functions

--- src/gui/dinodbusobject.hpp
+++ src/gui/dinodbusobject.hpp
@@ -88,25 +88,65 @@ protected:
   bool remove_tempo_change(int argc, DBus::Argument* argv);
   
   
+  /** Set the name of the track. */
   bool set_track_name(int argc, DBus::Argument* argv);
+  
+  /** Add a new pattern to the track. */
   bool add_pattern(int argc, DBus::Argument* argv);
+  
+  /** Create a copy of an existing pattern. */
   bool duplicate_pattern(int argc, DBus::Argument* argv);
+  
+  /** Remove a pattern from the track and delete it. */
   bool remove_pattern(int argc, DBus::Argument* argv);
+  
+  /** Add a new sequence entry in the track. */
   bool add_sequence_entry(int argc, DBus::Argument* argv);
+  
+  /** Remove a sequence entry from the track. */
   bool remove_sequence_entry(int argc, DBus::Argument* argv);
+  
+  /** Change the length of a sequence entry. */
   bool set_sequence_entry_length(int argc, DBus::Argument* argv);
+  
+  /** Change the MIDI channel that this track sends MIDI events to. */
   bool set_track_midi_channel(int argc, DBus::Argument* argv);
+  
+  /** Add a controller to this track. */
   bool add_controller(int argc, DBus::Argument* argv);
+  
+  /** Remove a controller from this track. */
   bool remove_controller(int argc, DBus::Argument* argv);
+  
+  /** Set the name of a controller. */
   bool set_controller_name(int argc, DBus::Argument* argv);
+  
+  /** Set the minimum value of a controller. */
   bool set_controller_min(int argc, DBus::Argument* argv);
+  
+  /** Set the maximum value of a controller. */
   bool set_controller_max(int argc, DBus::Argument* argv);
+  
+  /** Set the default value of a controller. */
   bool set_controller_default(int argc, DBus::Argument* argv);
+  
+  /** Set the ID number of a controller. */
   bool set_controller_number(int argc, DBus::Argument* argv);
+  
+  /** Set whether a controller has a single curve for the whole track
+      or one curve per pattern. */
   bool set_controller_global(int argc, DBus::Argument* argv);
+  
+  /** Add a named key. */
   bool add_key(int argc, DBus::Argument* argv);
+  
+  /** Remove a named key. */
   bool remove_key(int argc, DBus::Argument* argv);
+  
+  /** Set the name of a named key. */
   bool set_key_name(int argc, DBus::Argument* argv);
+  
+  /** Set the MIDI key number of a named key. */
   bool set_key_number(int argc, DBus::Argument* argv);
 
   bool set_pattern_name(int argc, DBus::Argument* argv);

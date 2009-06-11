7981872817adc5497a79d01cdf8b23349ea25b36
Lars Luthman <lars.luthman@gmail.com>
2009-06-11 14:43:10 +0200

    Added copyright notices to new source files

--- src/libdinoseq/getfirst.hpp
+++ src/libdinoseq/getfirst.hpp
@@ -1,3 +1,21 @@
+/*****************************************************************************
+    libdinoseq - a library for MIDI sequencing
+    Copyright (C) 2009  Lars Luthman <lars.luthman@gmail.com>
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*****************************************************************************/
+
 #ifndef GETFIRST_HPP
 #define GETFIRST_HPP
 
@@ -7,7 +25,9 @@ namespace Dino {
   
   
   /** This is a functor type template that returns the @c first member of a
-      pair when called. */
+      pair when called. It can be used to transform a map or multimap iterator
+      into a set-like iterator over the keys using boost::transform_iterator.
+  */
   template <typename K, typename V>
   struct GetFirst {
     typedef K result_type;

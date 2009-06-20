735bc8016620e41a92990b1ad871be2af83b07c7
Lars Luthman <lars.luthman@gmail.com>
2009-06-20 17:16:52 +0200

    Added AtomicInt

---
+++ src/libdinoseq/atomicint.hpp
@@ -0,0 +1,63 @@
+/*****************************************************************************
+    libdinoseq - a library for MIDI sequencing
+    Copyright (C) 2009  Lars Luthman <lars.luthman@gmail.com>
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*****************************************************************************/
+
+#ifndef ATOMICINT_HPP
+#define ATOMICINT_HPP
+
+#include <glib.h>
+
+namespace Dino {
+
+
+  /** An atomic integer class. It has two functions,
+      get() and set() that reads and writes the integer using atomic 
+      operations. They use the atomic operations defined in Glib, so they are
+      atomic and lock-free as long as Glib's operations are (should work on 
+      all common hardware platforms). They also act as memory barriers. This 
+      makes them well suited as building blocks for lock-free data structures.
+  */
+  class AtomicInt {
+  public:
+    
+    /** This is the underlying integer type for the AtomicInt class. */
+    typedef gint Type;
+    
+    
+    /** Initialise the atomic integer to the value of @c t. This operation is
+	@b not atomic. */
+    AtomicInt(Type value);
+    
+    /** Return the value of the atomic integer as a normal pinteger. This is
+	an atomic and lock-free operation, and it's also a memory barrier. */
+    Type get();
+    
+    /** Set the value of the atomic integer. This is an atomic and lock-free
+	operation and also a memory barrier. */
+    void set(Type new_value);
+    
+  private:
+    
+    Type m_data;
+    
+  };
+  
+
+}
+
+
+#endif
---
+++ src/test/libdinoseq/atomicint_test.cpp
@@ -0,0 +1,52 @@
+/*****************************************************************************
+    libdinoseq_test - unit test module for libdinoseq
+    Copyright (C) 2009  Lars Luthman <lars.luthman@gmail.com>
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*****************************************************************************/
+
+#include <boost/test/unit_test.hpp>
+
+#include "atomicint.hpp"
+
+
+using namespace Dino;
+
+
+/* We can't really test the atomicity in any reasonable way, so we just
+   do some trivial read and write tests. */
+
+BOOST_AUTO_TEST_SUITE(AtomicIntTest)
+
+
+BOOST_AUTO_TEST_CASE(constructor) {
+  AtomicInt::Type i = 3;
+  BOOST_CHECK_NO_THROW(AtomicInt ai(i));
+}
+
+
+BOOST_AUTO_TEST_CASE(get_set) {
+  AtomicInt::Type a = 42;
+  AtomicInt::Type b = 666;
+  AtomicInt ai = a;
+  
+  BOOST_CHECK_EQUAL(a, ai.get());
+  
+  ai.set(b);
+  
+  BOOST_CHECK_EQUAL(b, ai.get());
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
